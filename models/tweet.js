d8e0baf805dd926b8b4285c8d670da0228c7d8e6
Zachary J Rollyson <me@zjr.io>
2015-10-03 14:32:24 -0400

    Add tweet schema (coords broken).

--- models/tweet.js
+++ models/tweet.js
@@ -4,7 +4,32 @@ var mongoose = require('mongoose');
 
 var tweetModel = function () {
 
-  var tweetSchema = mongoose.Schema({});
+  var tweetSchema = mongoose.Schema({
+    id_str      : String,
+    text        : String,
+    created_at  : Date,
+    timestamp_ms: String,
+    lang        : String,
+    place       : Object, // possible 'place' tweeted from
+
+    coordinates : {
+      coordinates: [Number],
+      type       : String
+    },
+
+    user        : {
+      id_str            : String,
+      name              : String,
+      description       : String,
+      screen_name       : String,
+      location          : String, // user defined location
+      geo_enabled       : Boolean,
+      time_zone         : String, // user declared
+      utc_offset        : Number,
+      profile_image_url : String,
+      profile_banner_url: String
+    }
+  });
 
   return mongoose.model('Tweet', tweetSchema);
 
