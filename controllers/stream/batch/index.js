fca3e564171df758a907bb8f892440333ae8fef8
Zachary J Rollyson <me@zjr.io>
2015-10-05 09:14:16 -0400

    /stream/batch controller gets 20 streamed tweets.

--- controllers/stream/batch/index.js
+++ controllers/stream/batch/index.js
@@ -5,4 +5,30 @@ var Promise     = require('bluebird');
 var TweetStream = require('../../../lib/stream/tweet-stream');
 var Tweet       = require('../../../models/tweet');
 
+const count     = 20;
+
+module.exports  = function (router) {
+
+  router.get('/', function (req, res) {
+    var tweetPromises = [];
+    var tweetStream = new TweetStream();
+
+    tweetStream.on('data', data => {
+      tweetPromises.push(Tweet.create(data));
+
+      if (tweetPromises.length < count) { return; }
+
+      tweetStream.stop();
+
+      Promise
+        .all(tweetPromises)
+        .then(tweets => res.json(tweets))
+        .catch(err => res.json(err));
+    });
+
+    tweetStream.on('error', err => res.json(err));
+  });
+
+};
+
 
